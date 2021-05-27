56e822a75e8d531d573cab3a6c8c306c4db08d1c
Patrick <patrick.sweeney@protedyne.com>
2021-05-27 12:19:09 -0400

    Add initial pseudocode for download and parse

---
+++ createPodCast.sh
@@ -0,0 +1,21 @@
+#! bin/bash
+# Reference URL is https://archive.org/download/OA-2005-11
+
+echo "Audio source URL is $1" 
+
+# Download specified URL
+curl $1 > sourceList.htm
+
+# Prepare start of RSS file with initial podcast metadata
+
+# Search stored file for links to mp3 files
+
+    # Reconstruct path to mp3 file
+
+    # Prepare metadata for specified mp3 file
+
+    # Add mp3 file path to RSS file
+
+# Complete RSS file for proper podcast format
+
+
