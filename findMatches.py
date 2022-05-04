1ee5f9903572774f3fad3af0d48cc262aa55de21
thpdg <githubuser@hullbreach.com>
2022-05-04 20:46:53 +0000

    First Implementation - findMatches.py now loads templates and fills them with data from provided file

---
+++ findMatches.py
@@ -0,0 +1,78 @@
+# coding=utf8
+# the above tag defines encoding for this document and is for Python 2.x compatibility
+
+import re
+import sys
+
+filename = ""
+file_url = ""
+
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    file_url = sys.argv[2]
+
+print(f"Working with {filename} from {file_url}.")
+
+regex = r"<td><a href=\"(.+?\.mp3)\">(.+?\.mp3)<\/a><\/td>"
+title_regex = r"<h1>Files for (.+?)</h1>|$"
+
+# Load file from archive.org into string
+text_file = open(filename, "r")
+file_data = text_file.read()
+
+rss_title = re.findall(title_regex,file_data)
+print(f'Title is {rss_title[0]}')
+
+# Load RSS document template into string
+text_file = open("RSSFeedDocumentTemplate.xml", "r")
+output_document = text_file.read()
+
+# Load RSS item template into string
+text_file = open("RSSFeedItemTemplate.xml", "r")
+output_item_template = text_file.read()
+
+# test_str = ("<!DOCTYPE html>\n"
+# "    ")
+
+matches = re.finditer(regex, file_data, re.MULTILINE)
+
+episode_entries = ""
+
+def determine_date(source_string):
+    date_string = "2022-05-04"
+    return date_string
+
+for matchNum, match in enumerate(matches, start=1):
+    
+    # print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
+        episode_url = match.group(groupNum)
+        print(f"URL is {episode_url}")
+        
+        # print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+
+        episode_desc = match.group(groupNum)
+
+        if len(match.groups())>1:
+            episode_desc = match.group(2).replace('&amp;','&') 
+            print (f"Desc found: {episode_desc}")
+
+        episode_entry = output_item_template.replace("%EPISODE_TITLE%", episode_desc)
+        episode_entry = episode_entry.replace("%EPISODE_SUMMARY%", episode_desc)
+        episode_entry = episode_entry.replace("%EPISODE_DESCRIPTION%", episode_desc)
+        episode_entry = episode_entry.replace("%SOUNDFILE_URL%", episode_desc)
+        episode_entry = episode_entry.replace("%SOUNDFILE_DATE%", determine_date(episode_desc))
+
+        print(f"Episode Info:\n{episode_entry}")
+
+        episode_entries += episode_entry + "\n"
+
+        break
+
+output_document = output_document.replace("%PODCAST_CONTENT%",episode_entries)
+
+f = open("demofile3.txt", "w")
+f.write(output_document)
+f.close()
